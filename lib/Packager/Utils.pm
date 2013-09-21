846edf7e40abc3e8d6e9b8be9aba2a5fcdf78d14
Jens Rehsack <sno@netbsd.org>
2013-09-21 16:02:24 +0200

    initialize Packager::Utils

---
+++ lib/Packager/Utils.pm
@@ -0,0 +1,98 @@
+package Packager::Utils;
+
+use 5.008003;
+use strict;
+use warnings FATAL => 'all';
+
+our $VERSION = "0.001";
+
+use Moo;
+use MooX::Cmd;
+
+sub execute
+{
+    my $self = shift;
+    my ( $args, $chain ) = @_;
+    die "Need to specify a sub-command!\n";
+}
+
+=head1 NAME
+
+Packager::Utils - The great new Packager::Utils!
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Packager::Utils;
+
+    my $foo = Packager::Utils->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 AUTHOR
+
+Jens Rehsack, C<< <rehsack at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-packager-utils at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Packager-Utils>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Packager::Utils
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Packager-Utils>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Packager-Utils>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Packager-Utils>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Packager-Utils/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Jens Rehsack.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1;    # End of Packager::Utils
---
+++ lib/Packager/Utils/Cmd/Create/Cmd/Package.pm
@@ -0,0 +1,20 @@
+package Packager::Utils::Cmd::Create::Cmd::Package;
+
+use 5.008003;
+use strict;
+use warnings FATAL => 'all';
+
+use Moo;
+use MooX::Cmd;
+use MooX::Options;
+
+sub execute
+{
+    my $self = shift;
+
+    ...;
+
+    return 0;
+}
+
+1;
