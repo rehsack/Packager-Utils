11e1a1459aaa946a747890b717aa36f249ea9485
Jens Rehsack <sno@netbsd.org>
2013-12-02 08:23:57 +0100

    improve long_doc of report options

--- lib/Packager/Utils/Role/Report.pm
+++ lib/Packager/Utils/Role/Report.pm
@@ -12,19 +12,27 @@ use Template ();
 our $VERSION = '0.001';
 
 option 'output' => (
-                     is        => "ro",
-                     format    => "s@",
-                     doc       => "Desired output templates",
+                     is       => "ro",
+                     format   => "s@",
+                     doc      => "Desired output templates",
+                     long_doc => "Choose a template (or some more) for "
+                       . "output. The template name usually becomes part of "
+                       . "generated filename - usually as extension."
+                       . "Another filename can be chosen per template by assigning "
+                       . "a full qualified pathname to the output-target:\n\n"
+                       . '--output html-installed=`date "+%Y-%m-%d"`-foo.html',
                      autosplit => ",",
-                     short     => "t",
+                     short     => "o",
                      required  => 1,
                    );
 
 option 'target' => (
-                     is     => "lazy",
-                     format => "s",
-                     doc    => "Desired target location for processed templates",
-                     short  => "o",
+                     is       => "lazy",
+                     format   => "s",
+                     doc      => "Desired target location for processed templates",
+                     long_doc => "Choose a folder where output file(s) are generated.\n\n"
+                       . "\t--target /data/reports # default: \${HOME}",
+                     short => "t",
                    );
 
 sub _build_target
