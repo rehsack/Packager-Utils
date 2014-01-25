d48d6db12b7b5b2a8aec47d83884df591f07a541
Jens Rehsack <sno@netbsd.org>
2014-01-25 21:38:08 +0100

    add author, updated copyright notice and license

--- lib/Packager/Utils.pm
+++ lib/Packager/Utils.pm
@@ -33,30 +33,23 @@ Perhaps a little code snippet.
     my $foo = Packager::Utils->new();
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
 =head1 AUTHOR
 
 Jens Rehsack, C<< <rehsack at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-packager-utils at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Packager-Utils>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to
+C<bug-packager-utils at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Packager-Utils>.
+I will be notified, and then you'll automatically be notified of progress
+on your bug as I make changes.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc Packager::Utils
-
 
 You can also look for information at:
 
@@ -80,20 +73,18 @@ L<http://search.cpan.org/dist/Packager-Utils/>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
 
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013 Jens Rehsack.
+Copyright 2013,2014 Jens Rehsack.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
 See L<http://dev.perl.org/licenses/> for more information.
-
 
 =cut
 
--- lib/Packager/Utils/Role/Template.pm
+++ lib/Packager/Utils/Role/Template.pm
@@ -81,4 +81,59 @@ sub process_templates
     }
 }
 
+=head1 AUTHOR
+
+Jens Rehsack, C<< <rehsack at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-packager-utils at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Packager-Utils>.
+I will be notified, and then you'll automatically be notified of progress
+on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Packager::Utils
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Packager-Utils>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Packager-Utils>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Packager-Utils>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Packager-Utils/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013,2014 Jens Rehsack.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+=cut
+
 1;
